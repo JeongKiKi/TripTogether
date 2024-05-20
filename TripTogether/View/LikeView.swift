b36ac2a11fde30d297fde3837a0b83e31b4cb045
JeongKiKi <jkh001301@gmail.com>
2024-05-20 23:18:24 +0900

    docs: 문서 추가

---
+++ TripTogether/View/LikeView.swift
@@ -0,0 +1,20 @@
+//
+//  LikeView.swift
+//  TripTogether
+//
+//  Created by 정기현 on 2024/05/20.
+//
+
+import UIKit
+
+class LikeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TripTogether/View/MypageView.swift
@@ -0,0 +1,20 @@
+//
+//  MypageView.swift
+//  TripTogether
+//
+//  Created by 정기현 on 2024/05/20.
+//
+
+import UIKit
+
+class MypageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
