16f331e869f25c9731fc9a1e7d6cb12368313280
JeongKiKi <jkh001301@gmail.com>
2024-05-20 18:38:47 +0900

    Initial Commit

---
+++ TripTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripTogether
+//
+//  Created by 정기현 on 2024/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
