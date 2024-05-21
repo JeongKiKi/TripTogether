0410c348f01eca007613f2804e736197d282978b
JeongKiKi <jkh001301@gmail.com>
2024-05-22 00:05:23 +0900

    feat: 마이페이지 기본 ui구현

--- TripTogether/Controller/MypageViewController.swift
+++ TripTogether/Controller/MypageViewController.swift
@@ -8,19 +8,44 @@
 import UIKit
 
 class MypageViewController: UIViewController {
+    let mypageView = MypageView()
+    let dummyModel = DummyModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        title = "MyPage"
+        view = mypageView
+        mypageView.myPageTableView.register(MypageTableViewCell.self, forCellReuseIdentifier: "MyPageCell")
+        mypageView.myPageTableView.dataSource = self
+        mypageView.myPageTableView.delegate = self
+    }
+}
+
+extension MypageViewController: UITableViewDataSource, UITableViewDelegate, MypageTableViewCellDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyModel.dummy.count
     }
 
-    /*
-     // MARK: - Navigation
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyPageCell", for: indexPath) as? MypageTableViewCell else {
+            return UITableViewCell()
+        }
+        let post = dummyModel.dummy[indexPath.row]
+        cell.myPhotoSpot.image = post.photo
+        cell.myDescriptionLabel.text = post.description
+        cell.delegate = self
+        return cell
+    }
 
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
+    func didTapOptionButton(in cell: MypageTableViewCell) {
+        guard let indexPath = mypageView.myPageTableView.indexPath(for: cell) else { return }
+        let post = dummyModel.dummy[indexPath.row]
+        print("버튼: \(post.description)")
+        // Handle the like action (e.g., update the model, UI, etc.)
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
 }
--- TripTogether/View/MypageView.swift
+++ TripTogether/View/MypageView.swift
@@ -5,16 +5,130 @@
 //  Created by 정기현 on 2024/05/20.
 //
 
+import SnapKit
 import UIKit
 
 class MypageView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        setupUI()
     }
-    */
 
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    lazy var userName: UILabel = {
+        let lb = UILabel()
+        lb.text = "userName"
+        lb.font = .boldSystemFont(ofSize: 20)
+        lb.textAlignment = .center
+        return lb
+    }()
+
+    lazy var myTotalLikeInt: UILabel = {
+        let lb = UILabel()
+        lb.text = "30"
+        lb.textColor = .black
+        lb.textAlignment = .center
+        return lb
+    }()
+
+    lazy var myTotalLikeLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "받은 총 좋아요"
+        lb.textColor = .black
+        lb.font = .systemFont(ofSize: 10)
+        lb.textAlignment = .center
+        return lb
+    }()
+
+    lazy var othersTotalLikeInt: UILabel = {
+        let lb = UILabel()
+        lb.text = "30"
+        lb.textColor = .black
+        lb.textAlignment = .center
+        return lb
+    }()
+
+    lazy var othersTotalLikeLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "누른 좋아요"
+        lb.font = .systemFont(ofSize: 10)
+        lb.textColor = .black
+        lb.textAlignment = .center
+        return lb
+    }()
+
+    lazy var myStack: UIStackView = {
+        let st = UIStackView(arrangedSubviews: [myTotalLikeInt, myTotalLikeLabel])
+        st.axis = .vertical
+        st.alignment = .fill
+        st.distribution = .fillEqually
+        st.spacing = 7
+        addSubview(st)
+        return st
+    }()
+
+    lazy var othersStack: UIStackView = {
+        let st = UIStackView(arrangedSubviews: [othersTotalLikeInt, othersTotalLikeLabel])
+        st.axis = .vertical
+        st.alignment = .fill
+        st.distribution = .fillEqually
+        st.spacing = 7
+        addSubview(st)
+        return st
+    }()
+
+    lazy var likeStack: UIStackView = {
+        let st = UIStackView(arrangedSubviews: [myStack, othersStack])
+        st.axis = .horizontal
+        st.alignment = .fill
+        st.distribution = .fillEqually
+        st.spacing = 7
+        addSubview(st)
+        return st
+    }()
+
+    lazy var userinfo: UIView = {
+        let vw = UIView()
+        vw.addSubview(userName)
+        vw.addSubview(likeStack)
+        vw.backgroundColor = .yellow
+        return vw
+    }()
+
+    lazy var myPageTableView: UITableView = {
+        let table = UITableView()
+        return table
+    }()
+}
+
+extension MypageView {
+    func setupUI() {
+        addSubview(userinfo)
+        addSubview(myPageTableView)
+        userinfo.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(40)
+            $0.height.equalTo(100)
+        }
+        userName.snp.makeConstraints {
+            $0.top.equalTo(userinfo.snp.top).offset(10)
+            $0.leading.trailing.equalTo(userinfo)
+            $0.height.equalTo(30)
+        }
+        likeStack.snp.makeConstraints {
+            $0.top.equalTo(userName.snp.bottom).offset(10)
+            $0.leading.trailing.equalTo(userinfo).inset(20)
+            $0.bottom.equalTo(userinfo).offset(-10)
+        }
+        myPageTableView.snp.makeConstraints {
+            $0.top.equalTo(userinfo.snp.bottom).offset(20)
+            $0.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
+            $0.leading.trailing.equalTo(0)
+        }
+    }
 }
