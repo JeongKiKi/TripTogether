0dc447cab60941ad68dfa9ae72986573b6f5cc1a
JeongKiKi <jkh001301@gmail.com>
2024-05-21 21:32:35 +0900

    feat: 좋아요 페이지 컬렉션뷰 적용

--- TripTogether/View/LikeView.swift
+++ TripTogether/View/LikeView.swift
@@ -5,16 +5,32 @@
 //  Created by 정기현 on 2024/05/20.
 //
 
+import SnapKit
 import UIKit
-
 class LikeView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        setupUI()
     }
-    */
 
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    lazy var likeCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return cv
+    }()
+
+    func setupUI() {
+        addSubview(likeCollectionView)
+        likeCollectionView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top).offset(0)
+            $0.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
+            $0.leading.trailing.equalTo(0)
+        }
+    }
 }
