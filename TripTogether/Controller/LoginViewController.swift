227b4e28c58791e0c76185a05b0dfa297c4f739a
JeongKiKi <jkh001301@gmail.com>
2024-07-03 01:20:43 +0900

    docs: 주석추가

--- TripTogether/Controller/LoginViewController.swift
+++ TripTogether/Controller/LoginViewController.swift
@@ -27,25 +27,29 @@ class LoginViewController: UIViewController {
         view.backgroundColor = .appColor
     }
 
+    // 버튼들의 액션을 설정하는 함수
     private func setupActions() {
         loginView.loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
         loginView.makeEmailButton.addTarget(self, action: #selector(makeEmailButtonTapped), for: .touchUpInside)
         loginView.findPasswordButton.addTarget(self, action: #selector(findPasswordButtonTapped), for: .touchUpInside)
     }
 
+    // 로그인 버튼 클릭시 이벤트
     @objc private func loginButtonTapped() {
+        // 옵셔널 바인딩, 빈어있는지 확인
         guard let email = loginView.idTextField.text, !email.isEmpty,
               let password = loginView.passwordTextField.text, !password.isEmpty
         else {
             print("Email and password fields cannot be empty")
             return
         }
-
+        // Firebase Auth를 사용하여 로그인 시도
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
             guard let self = self else { return }
             if let error = error {
                 print("Error logging in: \(error.localizedDescription)")
                 print("아이디 비번 틀림")
+                // 로그인 실패 시 텍스트 필드를 비우고, placeholder와 오류 메시지를 업데이트
                 loginView.idTextField.text = ""
                 loginView.passwordTextField.text = ""
                 loginView.idTextField.placeholder = "아이디를 확인해주세요"
@@ -56,7 +60,11 @@ class LoginViewController: UIViewController {
             }
 
             print("Successfully logged in")
+
+            // 인증 결과에서 사용자 UID를 가져옴
             guard let uid = authResult?.user.uid else { return }
+
+            // 사용자 데이터를 가져오는 함수 호출
             self.fetchUserData(uid: uid)
         }
     }
@@ -100,7 +108,10 @@ class LoginViewController: UIViewController {
         let findVC = FindPasswordViewController()
         navigationController?.pushViewController(findVC, animated: true)
     }
+}
 
+// 이메일,비밀번호 모두 입력 시 로그인버튼 활성화를 위한 로직
+extension LoginViewController: UITextFieldDelegate {
     // FieldDelegates설정
     private func setupTextFieldDelegates() {
         loginView.idTextField.delegate = self
@@ -121,9 +132,7 @@ class LoginViewController: UIViewController {
         loginView.loginButton.isEnabled = isEmailFilled && isPasswordFilled
         loginBtnOn()
     }
-}
 
-extension LoginViewController: UITextFieldDelegate {
     // 텍스트 필드의 내용이 변경될 때마다 호출하여 버튼 상태 업데이트
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         DispatchQueue.main.async {
